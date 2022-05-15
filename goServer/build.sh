0abcb7115d686b9f172a93d8f53b61319806f90b
cond0r@desktop <zlc@zhao.lc>
2022-05-16 03:18:29 +0800

    编译和压缩goServer

--- goServer/build.sh
+++ goServer/build.sh
@@ -1,2 +1,2 @@
-go build -o ../bin/web_linux_amd64 .
-GOOS=windows go build -o ../bin/web_windows_amd64.exe .
+go build -ldflags "-s -w" -o ../bin/web_linux_amd64 .
+GOOS=windows go build -ldflags "-s -w" -o ../bin/web_windows_amd64.exe .
